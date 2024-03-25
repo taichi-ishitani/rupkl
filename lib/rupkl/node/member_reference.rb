1eacb1697134fe65437ea42c1d640477f14b99ff
Taichi Ishitani <taichi730@gmail.com>
2024-03-25 16:05:31 +0900

    separate definition

---
+++ lib/rupkl/node/member_reference.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module RuPkl
+  module Node
+    class MemberReference
+      def initialize(receiver, member, position)
+        @receiver = receiver
+        @member = member
+        @position = position
+      end
+
+      attr_reader :receiver
+      attr_reader :member
+      attr_reader :position
+
+      def evaluate(scopes)
+        member_node =
+          if receiver
+            find_member([receiver.evaluate(scopes)])
+          else
+            find_member(scopes)
+          end
+        member_node.evaluate(scopes).value
+      end
+
+      private
+
+      def find_member(scopes)
+        scopes.reverse_each do |scope|
+          node = scope&.properties&.find { _1.name.id == member.id }
+          return node if node
+        end
+
+        raise EvaluationError.new("cannot find property '#{member.id}'", position)
+      end
+    end
+  end
+end
